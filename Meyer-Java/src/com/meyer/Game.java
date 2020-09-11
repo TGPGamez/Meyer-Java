bf1404ad6d2f213ee3039e54ab59000e29a40d66
TGPGamez <tobi8268@zbc.dk>
2020-09-11 16:38:43 +0200

    Constructors

--- Meyer-Java/src/com/meyer/Game.java
+++ Meyer-Java/src/com/meyer/Game.java
@@ -1,6 +1,24 @@
 package com.meyer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
-    public Game()
+    private List<Player> players;
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public Game(String[] playersArray) {
+        players = MakePlayers(playersArray);
+    }
+
+    private List<Player> MakePlayers(String[] playersArray) {
+        List<Player> returnPlayers = new ArrayList<>();
+        for (int i = 0; i < playersArray.length; i++) {
+            returnPlayers.add(new Player(playersArray[i]));
+        }
+        return returnPlayers;
+    }
 }
--- Meyer-Java/src/com/meyer/Player.java
+++ Meyer-Java/src/com/meyer/Player.java
@@ -2,5 +2,15 @@ package com.meyer;
 
 public class Player {
 
+    private String name;
+    public void setName(String playerName) {
+        this.name = playerName;
+    }
+    public String getName() {
+        return name;
+    }
 
+    public Player(String playerName) {
+        setName(playerName);
+    }
 }
